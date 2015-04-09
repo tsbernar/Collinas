1911a09092f2fbfeeb0d31163807389b0abf913d
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-09 11:21:33 -0700

    trying to fix heroku db problems

--- db/migrate/initial_create_menu_items.rb
+++ db/migrate/initial_create_menu_items.rb
@@ -5,6 +5,7 @@ class CreateMenuItems < ActiveRecord::Migration
     	t.string :name, null: false
     	t.string :description, null: false
     	t.string :section, null: false
+      t.integer :menu_section_id
     	t.monetize :price
       t.timestamps null: false
     end
