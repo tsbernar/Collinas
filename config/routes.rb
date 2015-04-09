3f838c1d44d6fc3703d33ee5a2eac1692c1b54a9
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-09 06:43:59 -0700

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root: 'pages#home'
+  root 'pages#home'
   get "/" => "pages#home"
   get "/about" => "pages#about"
   get "/contact_us" => "pages#contact_us"
