ea3b1d286ffb1f97d7ca5a47b40c03135f075343
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-20 21:33:53 -0700

    fixed small error in braintree initializer to let env be a symbol

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,6 +1,5 @@
 
-## use for production 
-Braintree::Configuration.environment  = ENV['BRAINTREE_ENV']        
+Braintree::Configuration.environment  = ENV[BRAINTREE_ENV]        
 Braintree::Configuratio.merchant_id   = ENV['BRAINTREE_MERCHANT_ID'] 
 Braintree::Configuration.public_key   = ENV['BRAINTREE_PUBLIC_KEY']  
 Braintree::Configuration.private_key  = ENV['BRAINTREE_PRIVATE_KEY'] 
