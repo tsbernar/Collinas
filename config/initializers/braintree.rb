59c5937ec223fa31f094af30933ebe3d07fbe26a
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-22 19:17:08 -0700

    ditto

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,5 @@
 
-Braintree::Configuration.environment  = ENV[BRAINTREE_ENV]        
+Braintree::Configuration.environment  = :sandbox    
 Braintree::Configuratio.merchant_id   = ENV['BRAINTREE_MERCHANT_ID'] 
 Braintree::Configuration.public_key   = ENV['BRAINTREE_PUBLIC_KEY']  
 Braintree::Configuration.private_key  = ENV['BRAINTREE_PRIVATE_KEY'] 
