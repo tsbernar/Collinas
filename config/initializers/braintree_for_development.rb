8281eedd60b15726b3c280520fa6fa1893db5725
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-22 19:02:08 -0700

    ditto

--- config/initializers/braintree_for_development.rb
+++
@@ -1,4 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = '2kw26xkxmqpjcth7'
-Braintree::Configuration.public_key = 'yz9rwp4dn7j454qz'
-Braintree::Configuration.private_key = '6fee40f302953a4ecbd65b4b66a643ed'
