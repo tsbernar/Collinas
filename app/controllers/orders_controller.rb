cd926ef69a55311a70c30d99e8254ea9dddfc13e
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-04-23 20:45:17 -0700

    fixed checkout error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,16 @@ class OrdersController < ApplicationController
 
 	def create
 	  nonce = params[:payment_method_nonce]
+	  amount = current_cart.total.truncate(2).to_s
 	  render action: :new and return unless nonce
 	  result = Braintree::Transaction.sale(
-	    amount: "10.00",
-	    payment_method_nonce: nonce
+	    amount: amount,
+	    payment_method_nonce: nonce	  
 	  )
 
 	  flash[:notice] = "Transaction successful!" if result.success?
 	  flash[:alert] = "#{result.transaction.processor_response_text}" unless result.success?
-	  redirect_to order_path
+	  redirect_to root_path
 	end
 
 	def show
