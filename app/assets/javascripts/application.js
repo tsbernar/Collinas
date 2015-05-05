5e9046006df0fcd25bbe8f01f2308d05f0e2987b
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-05-05 14:17:18 -0700

    ..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require braintree
 //= require sync
+//= require bootstrap-sprockets
 //= require_tree .
