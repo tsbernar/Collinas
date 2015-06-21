fc34847d3121551f31194d56f8195ece6dc9ae15
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-06-20 19:50:54 -0700

    formating order email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,5 @@
 class OrderMailer < ApplicationMailer
-	default from: "info@bulletordering.com"
+	default from: "order@bulletordering.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,6 +8,6 @@ class OrderMailer < ApplicationMailer
   #
   def order_receipt(order)
   	@order = order
-    mail to: @order.email , subject: "Order ##{order.id}"
+    mail to: @order.email , subject: "Order ##{order.id}" , from: "order@bulletordering.com"
   end
 end
