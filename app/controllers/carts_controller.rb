4ded3d025492d1fcfe8fdae5634a2a3b3b72d037
Trevor Bernard <trevor.s.bernard@gmail.com>
2015-06-21 19:54:43 -0700

    added anchor for add to cart action on menu page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,7 @@ class CartsController < ApplicationController
 	def add_to_cart
 		@cart = current_cart
 	  @cart.add_item(params[:menu_item_id])
-	  redirect_to menu_items_path, notice: "Added to cart"
+	  redirect_to menu_items_path(:anchor => "menu", notice: "Added to cart")
 	end
 
 end
